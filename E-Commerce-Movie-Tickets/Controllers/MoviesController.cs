f549b43d8088d78546b0b6d62f91547ec1d7e3b6
Hanan-Nathem-Saadeh <hanan.nathem111@gmail.com>
2022-07-16 01:01:29 +0300

    ThirdBranch

---
+++ E-Commerce-Movie-Tickets/Controllers/MoviesController.cs
@@ -0,0 +1,21 @@
+﻿using E_Commerce_Movie_Tickets.Data;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using System.Threading.Tasks;
+
+namespace E_Commerce_Movie_Tickets.Controllers
+{
+    public class MoviesController : Controller
+    {
+        private readonly AppDbContext _context;
+        public MoviesController(AppDbContext context)
+        {
+            _context = context;
+        }
+        public async Task<IActionResult> Index()
+        {
+            var allMovies = await _context.Movies.ToListAsync();
+            return View();
+        }
+    }
+}
---
+++ E-Commerce-Movie-Tickets/Controllers/ProducersController.cs
@@ -0,0 +1,21 @@
+﻿using E_Commerce_Movie_Tickets.Data;
+using Microsoft.AspNetCore.Mvc;
+using System.Linq;
+
+namespace E_Commerce_Movie_Tickets.Controllers
+{
+    public class ProducersController : Controller
+    {
+        private readonly AppDbContext _context;
+        public ProducersController(AppDbContext context)
+        {
+            _context=context;
+        }
+
+        public IActionResult Index()
+        {
+            var allProducer = _context.Producers.ToList();
+            return View();
+        }
+    }
+}
