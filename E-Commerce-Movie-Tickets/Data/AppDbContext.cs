03481d144547bd5c8c6103f22a7860de0b5bb427
Hanan-Nathem-Saadeh <hanan.nathem111@gmail.com>
2022-07-15 23:38:11 +0300

    FirstBranch

---
+++ E-Commerce-Movie-Tickets/Data/AppDbContext.cs
@@ -0,0 +1,35 @@
+﻿using E_Commerce_Movie_Tickets.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace E_Commerce_Movie_Tickets.Data
+{
+    public class AppDbContext :DbContext
+    {
+        public AppDbContext(DbContextOptions<AppDbContext>options) : base(options)
+        {
+
+
+        }
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Actor_Movie>().HasKey(am => new
+            {
+                am.MovieId,
+                am.ActorId
+
+            });
+
+            base.OnModelCreating(modelBuilder);
+
+
+        }
+        public DbSet<Actor> Actors { get; set; }
+        public DbSet<Movie> Movies { get; set; }
+        public DbSet<Actor_Movie> Actors_Movies { get; set; }
+
+        public DbSet<Cinema> Cinemas { get; set; }
+
+        public DbSet<Producer> Producers { get; set; }
+
+    }
+}
--- E-Commerce-Movie-Tickets/Startup.cs
+++ E-Commerce-Movie-Tickets/Startup.cs
@@ -1,6 +1,8 @@
+using E_Commerce_Movie_Tickets.Data;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.HttpsPolicy;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
@@ -23,6 +25,12 @@ namespace E_Commerce_Movie_Tickets
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
+            services.AddDbContext<AppDbContext>(options =>
+            {
+                string connectionString = Configuration.GetConnectionString("DefaultConnection");
+                options.UseSqlServer(connectionString);
+            });
+            services.AddDbContext<AppDbContext>();
             services.AddControllersWithViews();
         }
 
